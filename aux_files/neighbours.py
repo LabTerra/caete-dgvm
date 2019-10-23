6de96eb0d75301e7568298799d8add0e46d11966
João Paulo Darela Filho <darelafilho@gmail.com>
2019-10-23 21:50:36 +0200

    upload simple scripts to aux_files: 1- to edit/create input data files for caetê; 2- Find the neighbours of an element in a 2d array

---
+++ aux_files/neighbours.py
@@ -0,0 +1,21 @@
+
+
+def neighbours(pos, matrix):
+    rows = len(matrix)
+    cols = len(matrix[0]) if rows else 0
+    for i in range(max(0, pos[0] - 1), min(rows, pos[0] + 2)):
+        for j in range(max(0, pos[1] - 1), min(cols, pos[1] + 2)):
+            if (i, j) != pos:
+                yield matrix[i][j]
+
+
+def neighbours_index(pos, matrix):
+    neighbours = []
+    rows = len(matrix)
+    cols = len(matrix[0]) if rows else 0
+    for i in range(max(0, pos[0] - 1), min(rows, pos[0] + 2)):
+        for j in range(max(0, pos[1] - 1), min(cols, pos[1] + 2)):
+            if (i, j) != pos:
+                neighbours.append((i,j))
+    return neighbours
+
